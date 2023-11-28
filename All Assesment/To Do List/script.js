b04a31b0063b5e72be67b1f46cb2012021f1f4dd
prerannaa <preranapandey322@gmail.com>
2023-11-28 23:49:57 +0545

    Added styling to github page

---
+++ All Assesment/To Do List/script.js
@@ -0,0 +1,79 @@
+
+function addTask() {
+  var taskInput = document.getElementById("taskInput");
+  var taskList = document.getElementById("taskList");
+  
+  var taskText = taskInput.value.trim();
+
+  if (taskText !== "") {
+    var li = document.createElement("li");
+
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", function() {
+      if (checkbox.checked) {
+        li.classList.add("completed");
+      } else {
+        li.classList.remove("completed");
+      }
+      filterTasks();
+    });
+
+    var textNode = document.createTextNode(taskText);
+    li.appendChild(checkbox);
+    li.appendChild(textNode);
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+
+    filterTasks(); // To update task visibility based on filters
+  } else {
+    alert("Please enter a task!");
+  }
+}
+
+function filterTasks(filterType) {
+  var tasks = document.getElementById("taskList").getElementsByTagName("li");
+
+  Array.from(tasks).forEach(function(task) {
+    var isCompleted = task.classList.contains("completed");
+    switch (filterType) {
+      case "completed":
+        task.style.display = isCompleted ? "block" : "none";
+        break;
+      case "remaining":
+        task.style.display = isCompleted ? "none" : "block";
+        break;
+      default:
+        task.style.display = "block";
+        break;
+    }
+  });
+}
+
+
+function searchTasks() {
+  var input, filter, ul, li, txtValue;
+  input = document.getElementById('searchInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById('taskList');
+  li = ul.getElementsByTagName('li');
+
+  let found = false;
+  Array.from(li).forEach(function(task) {
+    txtValue = task.textContent || task.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      task.style.display = '';
+      found = true;
+    } else {
+      task.style.display = 'none';
+    }
+  });
+
+  const notFoundMessage = document.getElementById('notFoundMessage');
+  if (!found) {
+    notFoundMessage.style.display = 'block';
+  } else {
+    notFoundMessage.style.display = 'none';
+  }
+}
