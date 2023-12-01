f62e0dc6f3676fa92ead14df47d7b85279572a67
prerannaa <preranapandey322@gmail.com>
2023-12-01 09:48:19 +0545

    Changes in viewport and radius of balls

--- All Assesment/Ball Collision/src/constants.js
+++ All Assesment/Ball Collision/src/constants.js
@@ -1,8 +1,8 @@
 
-const BALL_COUNT=10;
-const SPEED=5;
-const VIEWPORT_WIDTH=500;
+const BALL_COUNT=200;
+const SPEED=3;
+const VIEWPORT_WIDTH=1200;
 // document.body.ClientWidth
 const VIEWPORT_HEIGHT=500;
 const BALL_WIDTH=10;
-const BALL_HEIGHT=20;
+const BALL_HEIGHT=10;
