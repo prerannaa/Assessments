a19e6820162f0717ac2c3441e50af5ca9c935c8c
Prerana Pandey <power.intern@imark.com.np>
2023-12-11 14:41:53 +0545

    Implemented distance covered and collision detection mechaism along the terrain and car

---
+++ All Assesment/Hill Climb Racing/src/car.js
@@ -0,0 +1,87 @@
+const carImageSrc = 'assets/Car.png';
+const wheelImageSrc = 'assets/Tire.png';
+
+class Car {
+    constructor(position, carWidth, carHeight, wheelWidth, wheelHeight, terrain) {
+        this.position = position;
+        this.carWidth = carWidth;
+        this.carHeight = carHeight;
+        this.wheelWidth = wheelWidth;
+        this.wheelHeight = wheelHeight;
+        this.vx = 0; // Velocity along x axis
+        this.vy = 1; // Velocity along y axis
+        this.carImage = new Image();
+        this.carImage.src = carImageSrc;
+        this.wheelImage = new Image();
+        this.wheelImage.src = wheelImageSrc;
+        this.rotation = 0; // Initial rotation angle for wheels
+        this.rotationSpeed = 0.2; // Rotation speed for wheels
+        this.terrain = terrain;
+        this.distanceCovered = 0;
+        this.lastXPosition = position.x;
+
+    }
+
+    draw() {
+        // Draw car
+        ctx.drawImage(this.carImage, this.position.x, this.position.y, this.carWidth, this.carHeight);
+
+        // Draw wheels
+        const wheelY = this.position.y + this.carHeight - (this.wheelHeight/1.5);
+        const leftWheelX = this.position.x + this.carWidth * 0.07; // Adjust based on your desired position
+        const rightWheelX = this.position.x + this.carWidth * 0.94 - this.wheelWidth; // Adjust based on your desired position
+        this.drawWheel(leftWheelX, wheelY);
+        this.drawWheel(rightWheelX, wheelY);
+    }
+
+    drawWheel(x, y) {
+        ctx.save();
+        ctx.translate(x + this.wheelWidth / 2, y + this.wheelHeight / 2);
+        ctx.rotate(this.rotation);
+        ctx.drawImage(this.wheelImage, -this.wheelWidth /2, -this.wheelHeight / 2, this.wheelWidth, this.wheelHeight);
+        ctx.restore();
+    }
+
+    update() {
+        //this.draw();
+        this.drawDistance();
+        this.terrain.checkCollision(this);
+        this.position.y += this.vy;
+        if (this.position.y + this.wheelHeight + this.vy < canvas.height)
+        {
+            this.vy += GRAVITY
+        } 
+        else
+        {
+            this.vy = 0;
+            this.rotation += this.vx * this.rotationSpeed
+
+        }
+        this.position.x += this.vx
+
+        if (this.position.x > this.lastXPosition) {
+            this.distanceCovered += this.position.x - this.lastXPosition;
+            this.lastXPosition = this.position.x;
+        }
+
+        if (this.position.x > canvas.width/5){
+            const translationAmount = this.vx;
+            ctx.translate(-translationAmount, 0);
+        }
+
+        this.draw();
+
+
+    }
+
+    drawDistance(){
+        ctx.fillStyle = "white";
+        ctx.font = "16px Arial";
+        ctx.fillText("Distance: " + Math.round(this.distanceCovered), canvas.width - 120, 30);
+    }
+    
+
+    updateWheel(){
+        this.drawWheel()
+    }
+}
--- All Assesment/Hill Climb Racing/src/terrain.js
+++ All Assesment/Hill Climb Racing/src/terrain.js
@@ -1,40 +1,57 @@
-// terrain.js
+// // terrain.js
+// class Terrain {
+//     constructor() {
+//         this.columnWidth = 30; // Width of each terrain column
+//         this.frequency = 0.1; // Adjust the frequency of the curves
+//         this.amplitude = 60; // Adjust the amplitude of the curves
+//         this.offset = 200; // Adjust the starting position of the curves
+//         this.heights = this.generateHeights();
+//     }
+
+//     generateHeights() {
+//         const heights = [];
+//         const numColumns = Math.ceil(canvas.width / this.columnWidth);
+
+//         for (let i = 0; i < numColumns; i++) {
+//             // Generate curved height for each column using a sine function
+//             const x = i * this.frequency + this.offset;
+//             const height = canvas.height - this.amplitude * Math.sin(x);
+//             heights.push(height);
+//         }
+
+//         return heights;
+//     }
+    
+//     draw() {
+//         ctx.fillStyle = "#7cfc00"; // Set color for the terrain
+//         for (let i = 0; i < this.heights.length; i++) {
+//             ctx.fillRect(i * this.columnWidth, this.heights[i], this.columnWidth, canvas.height - this.heights[i]);
+//         }
+//     }
+    
+// }
+
 class Terrain {
     constructor() {
-        this.columnWidth = 30; // Width of each terrain column
-        this.frequency = 0.1; // Adjust the frequency of the curves
-        this.amplitude = 60; // Adjust the amplitude of the curves
-        this.offset = 200; // Adjust the starting position of the curves
-        this.heights = this.generateHeights();
+        this.terrainHeight = canvas.height - 100; // Adjust the height of the straight line
     }
 
-    generateHeights() {
-        const heights = [];
-        const numColumns = Math.ceil(canvas.width / this.columnWidth);
-
-        for (let i = 0; i < numColumns; i++) {
-            // Generate curved height for each column using a sine function
-            const x = i * this.frequency + this.offset;
-            const height = canvas.height - this.amplitude * Math.sin(x);
-            heights.push(height);
-        }
-
-        return heights;
-    }
-    
     draw() {
         ctx.fillStyle = "#7cfc00"; // Set color for the terrain
-        for (let i = 0; i < this.heights.length; i++) {
-            ctx.fillRect(i * this.columnWidth, this.heights[i], this.columnWidth, canvas.height - this.heights[i]);
+        ctx.fillRect(0, this.terrainHeight, canvas.width, canvas.height - this.terrainHeight);
+    }
+
+    checkCollision(car) {
+        const carBottomY = car.position.y + car.carHeight;
+
+        if (carBottomY > this.terrainHeight) {
+            // Collision with terrain
+            car.position.y = this.terrainHeight - car.carHeight;
+            car.vy = 0;
+            car.rotation += car.vx * car.rotationSpeed;
+
+            // Adjust car position based on terrain slope (for a straight line, no adjustment needed)
+            car.position.x += car.vx;
         }
     }
-    getHeight(x) {
-        // Interpolate the height based on the x position
-        const column = Math.floor(x / this.columnWidth);
-        const leftHeight = this.heights[column];
-        const rightHeight = this.heights[column + 1];
-        const t = (x % this.columnWidth) / this.columnWidth;
-        return leftHeight + t * (rightHeight - leftHeight);
-    }
-    
 }
